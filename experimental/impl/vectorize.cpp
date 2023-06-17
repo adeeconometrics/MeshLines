202e090fc4ee8a615a817f3d03bdd894d0208d97
ddamiana <amianism25@gmail.com>
2023-06-17 16:20:47 +0800

    checkpoint for using fold expression in eval

--- experimental/impl/vectorize.cpp
+++ experimental/impl/vectorize.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cassert>
 #include <functional>
 #include <iostream>
 #include <vector>
@@ -10,45 +11,64 @@ template <typename T> using Matrix = vector<vector<T>>;
 template <typename T, typename... Args>
 auto eval(const std::function<Matrix<T>(const Matrix<T> &)> &func,
           const Matrix<T> &matrix, Args &&... args) -> Matrix<T> {
-  if constexpr (sizeof...(Args) == 0) {
-    return func(matrix);
-  } else {
-    return eval(func, func(matrix), std::forward<Args>(args)...);
-  }
+  return (func(matrix), ..., func(std::forward<Args>(args)));
 }
 
-// template <typename T, typename U = T>
-// constexpr auto operator+(const Matrix<T> &lhs, const Matrix<U> &rhs)
-//     -> Matrix<common_type_t<T, U>> {
-//   Matrix<common_type_t<T, U>> result(lhs.size(),
-//   std::vector<T>(lhs[0].size()));
+template <typename T>
+constexpr auto apply(const std::initializer_list<std::vector<T>> &t_list,
+                     const std::function<T(std::initializer_list<T>)> &fn)
+    -> vector<T> {
 
-//   assert(lhs.size() == rhs.size());
+  const std::size_t first_size = t_list.begin()->size();
+  assert(std::all_of(t_list.cbegin(), t_list.cend(),
+                     [](auto v) -> bool { return v.size() == first_size; }));
 
-//   std::transform(lhs.cbegin(), lhs.cend(), rhs.cbegin(), result.begin(),
-//                  [](const vector<T> &a, const vector<U> &b) { return a + b;
-//                  });
+  std::vector<T> result;
+  result.reserve(first_size);
 
-//   return result;
-// }
+  for (std::size_t i = 0; i < first_size; i++) {
+    // result[i] = fn(~elements ~);
+    result[i] = fn({t_list});
+  }
+
+  return result;
+}
+
+template <typename T>
+constexpr auto operator+(const vector<T> &lhs, const vector<T> &rhs)
+    -> vector<T> {
+  assert(lhs.size() == rhs.size());
+  vector<T> result;
+  result.reserve(lhs.size());
+
+  std::transform(std::cbegin(lhs), std::cend(lhs), std::cbegin(rhs),
+                 std::back_inserter(result), std::plus<T>());
+  return result;
+}
+
+template <typename T>
+constexpr auto operator+(const Matrix<T> &lhs, const Matrix<T> &rhs)
+    -> Matrix<T> {
+  Matrix<T> result(lhs.size(), std::vector<T>(lhs[0].size()));
+
+  assert(lhs.size() == rhs.size());
+
+  std::transform(lhs.cbegin(), lhs.cend(), rhs.cbegin(), result.begin(),
+                 [](const vector<T> &a, const vector<T> &b) { return a + b; });
+
+  return result;
+}
 
 auto main() -> int {
   Matrix<int> matrix1 = {{1, 2}, {3, 4}};
   Matrix<int> matrix2 = {{5, 6}, {7, 8}};
   Matrix<int> matrix3 = {{9, 10}, {11, 12}};
 
-  std::function<Matrix<int>(const Matrix<int> &)> multiplyByTwo =
-      [](const Matrix<int> &matrix) {
-        Matrix<int> result = matrix;
-        for (auto &row : result) {
-          for (auto &element : row) {
-            element *= 2;
-          }
-        }
-        return result;
-      };
+  std::function<Matrix<int>(const Matrix<int> &)> addFunc =
+      [](const Matrix<int> &m) { return m; };
 
-  Matrix<int> result = eval(multiplyByTwo, matrix1, matrix2, matrix3);
+  Matrix<int> result = eval(addFunc, matrix1, matrix2, matrix3);
+  // Evaluates as: addFunc(matrix1, addFunc(matrix2, addFunc(matrix3)))
 
   for (const auto &row : result) {
     for (const auto &element : row) {
@@ -56,6 +76,5 @@ auto main() -> int {
     }
     std::cout << std::endl;
   }
-
   return 0;
 }
