6363b320e8224fc3ccdd682452365a4d93179311
ddamiana <amianism25@gmail.com>
2023-06-17 17:11:03 +0800

    revised version of apply

--- experimental/impl/vectorize.cpp
+++ experimental/impl/vectorize.cpp
@@ -1,80 +1,54 @@
 #include <algorithm>
 #include <cassert>
 #include <functional>
+#include <initializer_list>
 #include <iostream>
 #include <vector>
 
+using std::all_of;
+using std::cout;
+using std::function;
+using std::initializer_list;
 using std::vector;
 
-template <typename T> using Matrix = vector<vector<T>>;
-
-template <typename T, typename... Args>
-auto eval(const std::function<Matrix<T>(const Matrix<T> &)> &func,
-          const Matrix<T> &matrix, Args &&... args) -> Matrix<T> {
-  return (func(matrix), ..., func(std::forward<Args>(args)));
-}
-
 template <typename T>
-constexpr auto apply(const std::initializer_list<std::vector<T>> &t_list,
-                     const std::function<T(std::initializer_list<T>)> &fn)
-    -> vector<T> {
+constexpr auto apply(const initializer_list<vector<T>> &t_list,
+                     const function<T(initializer_list<T>)> &fn) -> vector<T> {
 
   const std::size_t first_size = t_list.begin()->size();
-  assert(std::all_of(t_list.cbegin(), t_list.cend(),
-                     [](auto v) -> bool { return v.size() == first_size; }));
+  assert(all_of(std::cbegin(t_list), std::cend(t_list),
+                [](const auto a) { return a.size() == first_size; }));
 
-  std::vector<T> result;
+  vector<T> result;
   result.reserve(first_size);
 
-  for (std::size_t i = 0; i < first_size; i++) {
-    // result[i] = fn(~elements ~);
-    result[i] = fn({t_list});
-  }
-
-  return result;
-}
-
-template <typename T>
-constexpr auto operator+(const vector<T> &lhs, const vector<T> &rhs)
-    -> vector<T> {
-  assert(lhs.size() == rhs.size());
-  vector<T> result;
-  result.reserve(lhs.size());
-
-  std::transform(std::cbegin(lhs), std::cend(lhs), std::cbegin(rhs),
-                 std::back_inserter(result), std::plus<T>());
-  return result;
-}
-
-template <typename T>
-constexpr auto operator+(const Matrix<T> &lhs, const Matrix<T> &rhs)
-    -> Matrix<T> {
-  Matrix<T> result(lhs.size(), std::vector<T>(lhs[0].size()));
-
-  assert(lhs.size() == rhs.size());
-
-  std::transform(lhs.cbegin(), lhs.cend(), rhs.cbegin(), result.begin(),
-                 [](const vector<T> &a, const vector<T> &b) { return a + b; });
+  for (const auto &vec : t_list)
+    for (const auto &element : vec)
+      result.insert(std::end(result), fn({element}));
 
   return result;
 }
 
 auto main() -> int {
-  Matrix<int> matrix1 = {{1, 2}, {3, 4}};
-  Matrix<int> matrix2 = {{5, 6}, {7, 8}};
-  Matrix<int> matrix3 = {{9, 10}, {11, 12}};
+  std::initializer_list<std::vector<int>> args = {
+      {1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-  std::function<Matrix<int>(const Matrix<int> &)> addFunc =
-      [](const Matrix<int> &m) { return m; };
+  std::function<int(std::initializer_list<int>)> increment =
+      [](std::initializer_list<int> elements) {
+        int result = 0;
+        for (const auto &element : elements) {
+          result += element + 1;
+        }
+        return result;
+      };
 
-  Matrix<int> result = eval(addFunc, matrix1, matrix2, matrix3);
-  // Evaluates as: addFunc(matrix1, addFunc(matrix2, addFunc(matrix3)))
+  std::vector<int> result = apply(args, increment);
 
-  for (const auto &row : result) {
-    for (const auto &element : row) {
-      std::cout << element << " ";
-    }
-    std::cout << std::endl;
+  // Print the result
+  for (const auto &element : result) {
+    std::cout << element << " ";
   }
+  std::cout << std::endl;
+
   return 0;
 }
