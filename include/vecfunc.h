317e7b38905b89b054978655e09a1bb60fd3675b
Dave Amiana <amianism25@gmail.com>
2024-05-19 11:03:09 +0800

    removed static_asserts and used conditional template instantiation

--- include/vecfunc.h
+++ include/vecfunc.h
@@ -27,20 +27,21 @@ template <typename T> constexpr auto dist(const vector<T> &v) -> double {
   return std::sqrt(result);
 }
 
-template <typename T> constexpr auto ones(size_t t_size) -> vector<T> {
-  assert(std::is_arithmetic_v<T>);
+template <typename T,
+          typename = typename std::enable_if_t<std::is_arithmetic_v<T>>>
+constexpr auto ones(size_t t_size) -> vector<T> {
   return vector<T>(1, t_size);
 }
 
-template <typename T> constexpr auto zeros(size_t t_size) -> vector<T> {
-  assert(std::is_arithmetic_v<T>);
+template <typename T,
+          typename = typename std::enable_if_t<std::is_arithmetic_v<T>>>
+constexpr auto zeros(size_t t_size) -> vector<T> {
   return vector<T>(0, t_size);
 }
 
-template <typename T>
+template <typename T,
+          typename = typename std::enable_if_t<std::is_arithmetic_v<T>>>
 constexpr auto lp_norm(const vector<T> &v, float p) -> float {
-  static_assert(std::is_arithmetic_v<T>,
-                "template parameter must be of type arithmetic");
   if (p == 0.0) {
     return std::count_if(v.cbegin(), v.cend(),
                          [](const auto &x) -> bool { return x != 0; });
@@ -63,9 +64,9 @@ constexpr auto lp_norm(const vector<T> &v, float p) -> float {
   return std::pow(result, 1 / p);
 };
 
-template <typename T> constexpr auto sum(std::vector<T> &v) -> T {
-  static_assert(std::is_arithmetic_v<T>,
-                "template parameter must be of type arithmetic");
+template <typename T,
+          typename = typename std::enable_if_t<std::is_arithmetic_v<T>>>
+constexpr auto sum(std::vector<T> &v) -> T {
 
   T result{};
   std::for_each(v.cbegin(), v.cend(),
@@ -73,9 +74,9 @@ template <typename T> constexpr auto sum(std::vector<T> &v) -> T {
   return result;
 }
 
-template <typename T> constexpr auto prod(std::vector<T> &v) -> T {
-  static_assert(std::is_arithmetic_v<T>,
-                "template parameter must be of type arithmetic");
+template <typename T,
+          typename = typename std::enable_if_t<std::is_arithmetic_v<T>>>
+constexpr auto prod(std::vector<T> &v) -> T {
 
   T result{};
   std::for_each(v.cbegin(), v.cend(),
@@ -84,23 +85,29 @@ template <typename T> constexpr auto prod(std::vector<T> &v) -> T {
 }
 
 template <typename T, typename U = T>
-constexpr auto dot(const vector<T> &lhs, const vector<U> &rhs)
-    -> vector<common_type_t<T, U>> {
+constexpr auto dot(const vector<T> &lhs,
+                   const vector<U> &rhs) -> vector<common_type_t<T, U>> {
   static_assert(std::is_arithmetic_v<T> && std::is_arithmetic_v<U>,
                 "template parameters must be of type arithmetic");
   return sum(rhs * lhs);
 }
 
 template <typename T, typename U = T>
-constexpr auto get_angle(const vector<T> &lhs, const vector<U> &rhs)
-    -> common_type_t<T, U> {
-  common_type_t<T, U> result;
+constexpr auto get_angle(const vector<T> &lhs,
+                         const vector<U> &rhs) -> common_type_t<T, U> {
   return std::acos(dot(lhs, rhs) / (dist(lhs) * dist(rhs)));
 }
-
+/**
+ * @brief
+ *
+ * @tparam T
+ * @param v
+ * @return vector<std::common_type<T, double>>
+ */
 template <typename T>
-constexpr auto normalize(const vector<T> &v) -> vector<T> {
-  return v / dist(v); // check for condition of [int]/[double] and [int]/[int]
+constexpr auto
+normalize(const vector<T> &v) -> vector<std::common_type<T, double>> {
+  return v / dist(v);
 }
 } // namespace lin
 #endif // __VECFUNC_H__
