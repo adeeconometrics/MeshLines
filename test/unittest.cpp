d070abce2f14bce3f65b9fd3dbd48b8b49538738
Dave Amiana <amianism25@gmail.com>
2024-05-26 23:23:59 +0800

    correct cholesky impl

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -493,3 +493,13 @@ TEST(MatDecompose, LUGaussian) {
     }
   }
 }
+
+TEST(MatDecompose, Cholesky) {
+  const Matrix<double, 3, 3> M = {
+      {4., 12., -16.}, {12., 37., -43.}, {-16., -43., 98.}};
+  const Matrix<double, 3, 3> L = {{2., 0., 0.}, {6., 1., 0.}, {-8., 5., 3.}};
+
+  const auto L_res = cholesky(M);
+
+  EXPECT_EQ(L_res, L);
+}
